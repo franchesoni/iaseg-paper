112d8aa982af8f12504cf28a719d16b6f5b47a85
franchesoni <marchesoniacland@gmail.com>
2023-03-27 17:27:03 +0200

    format

--- annotate_data.py
+++ annotate_data.py
@@ -13,104 +13,148 @@ from app.ClickSEG.clean_inference import load_controller
 from app.ClickSEG.isegm.inference.clicker import Clicker
 from config import DATA_DIR
 
+
 def get_last_ckpt_path(ckpt_path):
-  """Get the path of the last checkpoint in a directory"""
-  assert isinstance(ckpt_path, Path)
-  if ckpt_path.is_dir():
-    ckpts = sorted(ckpt_path.glob('*.pth'))
-    if len(ckpts):
-      return ckpts[-1]
+    """Get the path of the last checkpoint in a directory"""
+    assert isinstance(ckpt_path, Path)
+    if ckpt_path.is_dir():
+        ckpts = sorted(ckpt_path.glob("*.pth"))
+        if len(ckpts):
+            return ckpts[-1]
+        else:
+            return ckpt_path
     else:
-      return ckpt_path
-  else:
-    return sorted(ckpt_path.parent.glob('*.pth'))[-1]
+        return sorted(ckpt_path.parent.glob("*.pth"))[-1]
+
 
 def IoU(mask1, mask2):
-  """Compute the intersection over union of two masks"""
-  intersection = np.logical_and(mask1, mask2)
-  union = np.logical_or(mask1, mask2)
-  return np.sum(intersection) / np.sum(union)
+    """Compute the intersection over union of two masks"""
+    intersection = np.logical_and(mask1, mask2)
+    union = np.logical_or(mask1, mask2)
+    return np.sum(intersection) / np.sum(union)
+
 
 def mytorchload(ckpt_path):
     """Just like torch.load but checking that the file is ready"""
-    lock_file_path = ckpt_path + '.lock'
+    lock_file_path = ckpt_path + ".lock"
     if os.path.exists(lock_file_path):
         time.sleep(0.5)  # this is more than enough
     return torch.load(ckpt_path)
 
+
 def get_updated_model(ckpt_path, model=None):
-  """Reload the model if checkpoint has been updated"""
-  assert isinstance(ckpt_path, Path)
-  if ckpt_path.exists():
-      last_ckpt_path = get_last_ckpt_path(ckpt_path)
-      need_update = last_ckpt_path != ckpt_path or model is None
-      if need_update and not last_ckpt_path.is_dir():
-        return mytorchload(last_ckpt_path), last_ckpt_path
-      return model, last_ckpt_path
-  return model, ckpt_path
+    """Reload the model if checkpoint has been updated"""
+    assert isinstance(ckpt_path, Path)
+    if ckpt_path.exists():
+        last_ckpt_path = get_last_ckpt_path(ckpt_path)
+        need_update = last_ckpt_path != ckpt_path or model is None
+        if need_update and not last_ckpt_path.is_dir():
+            return mytorchload(last_ckpt_path), last_ckpt_path
+        return model, last_ckpt_path
+    return model, ckpt_path
+
 
 def annotate_img(img, preannotation, target, max_delay, threshold=0.9):
-  external_clicker = Clicker(gt_mask=np.array(target))
+    external_clicker = Clicker(gt_mask=np.array(target))
 
-  modelname = 'focalclick'
-  controller = load_controller()
-  assert controller is not None
+    modelname = "focalclick"
+    controller = load_controller()
+    assert controller is not None
 
-  # inference
-  controller.set_image(np.array(img))
-  pred_mask = np.zeros_like(np.array(target))
-  if preannotation is not None:
-    controller.set_mask(preannotation)
-    pred_mask = preannotation
-
-  number_of_clicks = 0
-  if np.sum(target) == 0:
-    if np.abs(np.sum(pred_mask)) > 0.001:
-      time.sleep(max_delay * np.random.rand())  # delay because we need to clear the bad prediction
+    # inference
+    controller.set_image(np.array(img))
     pred_mask = np.zeros_like(np.array(target))
-  else:
-    while IoU(pred_mask, target) < threshold and number_of_clicks < 20 and np.sum(target) != 0:
-      time.sleep(max_delay * np.random.rand())  # delay
-      external_clicker.make_next_click(pred_mask)
-      clicks_so_far = external_clicker.get_clicks()
-      last_click = clicks_so_far[-1]
-      (y, x) = last_click.coords
-      is_positive = last_click.is_positive
-      controller.add_click(x, y, is_positive)
-      pred_mask = controller.result_mask
-      number_of_clicks += 1
-  time.sleep(max_delay * 0.1)  # time to say 'next'
+    if preannotation is not None:
+        controller.set_mask(preannotation)
+        pred_mask = preannotation
 
-  return pred_mask
+    number_of_clicks = 0
+    if np.sum(target) == 0:
+        if np.abs(np.sum(pred_mask)) > 0.001:
+            time.sleep(
+                max_delay * np.random.rand()
+            )  # delay because we need to clear the bad prediction
+        pred_mask = np.zeros_like(np.array(target))
+    else:
+        while (
+            IoU(pred_mask, target) < threshold
+            and number_of_clicks < 20
+            and np.sum(target) != 0
+        ):
+            time.sleep(max_delay * np.random.rand())  # delay
+            external_clicker.make_next_click(pred_mask)
+            clicks_so_far = external_clicker.get_clicks()
+            last_click = clicks_so_far[-1]
+            (y, x) = last_click.coords
+            is_positive = last_click.is_positive
+            controller.add_click(x, y, is_positive)
+            pred_mask = controller.result_mask
+            number_of_clicks += 1
+    time.sleep(max_delay * 0.1)  # time to say 'next'
+
+    return pred_mask
 
 
-def annotate_data(input_dir, target_dir, annotations_dir, ckpt_path=Path('checkpoints'), label=10, max_delay=4):
-  Path(annotations_dir).mkdir(exist_ok=True, parents=True)
-  totensor = torchvision.transforms.ToTensor()
-  dataset = PerClassDataset(input_dir, target_dir, label)
-  for i, (img, target) in enumerate(dataset):
-    # check that the annotation does not already exist
-    if (Path(annotations_dir) / f"annotation_{str(i).zfill(6)}_{str(label).zfill(2)}.png").exists():
-      raise RuntimeError(f"Annotation {i} already exists")
-    # update the model
-    model, ckpt_path = get_updated_model(ckpt_path)
-    # annotate the image
-    preannotation = None
-    if model is not None:
-      device = next(model.decoder.parameters()).device
-      preannotation = model(torchvision.transforms.functional.resize(totensor(img).to(device), (480, 480), antialias=True).unsqueeze(0))
-      # resize back to img size
-      preannotation = torchvision.transforms.functional.resize(preannotation.squeeze(0), (img.height, img.width), antialias=True).detach().cpu().numpy()[0]
-    annotation = annotate_img(img, preannotation, target, max_delay)
-    # save the annotation
-    Image.fromarray(annotation).save(Path(annotations_dir) / f"annotation_{str(i).zfill(6)}_{str(label).zfill(2)}.png")
-    print("Annotated image", i, "with label", label, "and checkpoint", ckpt_path, "at time", datetime.datetime.utcnow())
+def annotate_data(
+    input_dir,
+    target_dir,
+    annotations_dir,
+    ckpt_path=Path("checkpoints"),
+    label=10,
+    max_delay=4,
+):
+    Path(annotations_dir).mkdir(exist_ok=True, parents=True)
+    totensor = torchvision.transforms.ToTensor()
+    dataset = PerClassDataset(input_dir, target_dir, label)
+    for i, (img, target) in enumerate(dataset):
+        # check that the annotation does not already exist
+        if (
+            Path(annotations_dir)
+            / f"annotation_{str(i).zfill(6)}_{str(label).zfill(2)}.png"
+        ).exists():
+            raise RuntimeError(f"Annotation {i} already exists")
+        # update the model
+        model, ckpt_path = get_updated_model(ckpt_path)
+        # annotate the image
+        preannotation = None
+        if model is not None:
+            device = next(model.decoder.parameters()).device
+            preannotation = model(
+                torchvision.transforms.functional.resize(
+                    totensor(img).to(device), (480, 480), antialias=True
+                ).unsqueeze(0)
+            )
+            # resize back to img size
+            preannotation = (
+                torchvision.transforms.functional.resize(
+                    preannotation.squeeze(0), (img.height, img.width), antialias=True
+                )
+                .detach()
+                .cpu()
+                .numpy()[0]
+            )
+        annotation = annotate_img(img, preannotation, target, max_delay)
+        # save the annotation
+        Image.fromarray(annotation).save(
+            Path(annotations_dir)
+            / f"annotation_{str(i).zfill(6)}_{str(label).zfill(2)}.png"
+        )
+        print(
+            "Annotated image",
+            i,
+            "with label",
+            label,
+            "and checkpoint",
+            ckpt_path,
+            "at time",
+            datetime.datetime.utcnow(),
+        )
 
 
 if __name__ == "__main__":
-  # get label as first argumetn of call
-  label = int(sys.argv[1])
-  input_dir = os.path.join(DATA_DIR, 'perlabel_sbd/inputs')
-  target_dir = os.path.join(DATA_DIR, 'perlabel_sbd/targets')
-  annotations_dir = os.path.join(DATA_DIR, 'perlabel_sbd/annotations')
-  annotate_data(input_dir, target_dir, annotations_dir, label=label, max_delay=4)
+    # get label as first argumetn of call
+    label = int(sys.argv[1])
+    input_dir = os.path.join(DATA_DIR, "perlabel_sbd/inputs")
+    target_dir = os.path.join(DATA_DIR, "perlabel_sbd/targets")
+    annotations_dir = os.path.join(DATA_DIR, "perlabel_sbd/annotations")
+    annotate_data(input_dir, target_dir, annotations_dir, label=label, max_delay=4)
--- continuously_train.py
+++ continuously_train.py
@@ -15,7 +15,8 @@ from torch.utils.tensorboard import SummaryWriter
 
 from config import DATA_DIR
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 def seed_everything(seed=42):
     random.seed(seed)
@@ -24,154 +25,194 @@ def seed_everything(seed=42):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
 
+
 def debug_id(x, **kwargs):
-  print(x.shape)
-  breakpoint()
-  return x
+    print(x.shape)
+    breakpoint()
+    return x
+
 
 class ContinuousDataset(torch.utils.data.Dataset):
-  def __init__(self, input_dir, annotations_dir, label, min_length=32):
-    self.input_dir = Path(input_dir)
-    self.annotations_dir = Path(annotations_dir)
-    self.label = label
+    def __init__(self, input_dir, annotations_dir, label, min_length=32):
+        self.input_dir = Path(input_dir)
+        self.annotations_dir = Path(annotations_dir)
+        self.label = label
 
-    self.min_length = min_length
-    self.update_file_list()  # receives min_length
-    self.aug = A.Compose([  # we could add more augmentations here
-        # A.Lambda(image=debug_id, mask=debug_id),
-        A.OneOf([
-          A.PadIfNeeded(min_height=480, min_width=480),
-          A.PadIfNeeded(min_height=960, min_width=960, p=0.2),
-        ], p=1),
-        A.RandomSizedCrop(min_max_height=(88, 480), height=480, width=480),  # sbd params
-        A.HorizontalFlip(p=0.5),
-        A.OneOf([
-            A.ElasticTransform(),
-            A.GridDistortion(),
-            A.OpticalDistortion()                  
-            ], p=0.3),
-        A.OneOf([
-          A.CLAHE(),
-          A.RandomBrightnessContrast(),    
-          A.RandomGamma()], p=0.3),
-        A.Normalize(),
-        ToTensorV2(),
-    ])
+        self.min_length = min_length
+        self.update_file_list()  # receives min_length
+        self.aug = A.Compose(
+            [  # we could add more augmentations here
+                # A.Lambda(image=debug_id, mask=debug_id),
+                A.OneOf(
+                    [
+                        A.PadIfNeeded(min_height=480, min_width=480),
+                        A.PadIfNeeded(min_height=960, min_width=960, p=0.2),
+                    ],
+                    p=1,
+                ),
+                A.RandomSizedCrop(
+                    min_max_height=(88, 480), height=480, width=480
+                ),  # sbd params
+                A.HorizontalFlip(p=0.5),
+                A.OneOf(
+                    [A.ElasticTransform(), A.GridDistortion(), A.OpticalDistortion()],
+                    p=0.3,
+                ),
+                A.OneOf(
+                    [A.CLAHE(), A.RandomBrightnessContrast(), A.RandomGamma()], p=0.3
+                ),
+                A.Normalize(),
+                ToTensorV2(),
+            ]
+        )
 
-  def update_file_list(self):
-    img_indices = [p.stem.split('_')[1] for p in self.input_dir.glob("img_*.png")]
-    annotation_indices = [ann.name.split('_')[1] for ann in self.annotations_dir.glob(f"annotation_*_{str(self.label).zfill(2)}.png")]
-    self.common_indices = sorted(list(set(img_indices).intersection(set(annotation_indices))))
-    if len(self.common_indices):  # shouldn't be 0
-      self.common_indices = self.common_indices * (self.min_length // len(self.common_indices) + 1)
-    return self.common_indices
+    def update_file_list(self):
+        img_indices = [p.stem.split("_")[1] for p in self.input_dir.glob("img_*.png")]
+        annotation_indices = [
+            ann.name.split("_")[1]
+            for ann in self.annotations_dir.glob(
+                f"annotation_*_{str(self.label).zfill(2)}.png"
+            )
+        ]
+        self.common_indices = sorted(
+            list(set(img_indices).intersection(set(annotation_indices)))
+        )
+        if len(self.common_indices):  # shouldn't be 0
+            self.common_indices = self.common_indices * (
+                self.min_length // len(self.common_indices) + 1
+            )
+        return self.common_indices
 
-  def __len__(self):
-    return len(self.common_indices)
+    def __len__(self):
+        return len(self.common_indices)
 
-  def __getitem__(self, idx):
-    image = np.array(Image.open(self.input_dir / f"img_{self.common_indices[idx]}.png"))
-    mask = (np.array(Image.open(self.annotations_dir / f"annotation_{self.common_indices[idx]}_{str(self.label).zfill(2)}.png"))!=0).astype(np.uint8)
-    augmented = self.aug(image=image, mask=mask)
-    return augmented['image'], augmented['mask']
+    def __getitem__(self, idx):
+        image = np.array(
+            Image.open(self.input_dir / f"img_{self.common_indices[idx]}.png")
+        )
+        mask = (
+            np.array(
+                Image.open(
+                    self.annotations_dir
+                    / f"annotation_{self.common_indices[idx]}_{str(self.label).zfill(2)}.png"
+                )
+            )
+            != 0
+        ).astype(np.uint8)
+        augmented = self.aug(image=image, mask=mask)
+        return augmented["image"], augmented["mask"]
+
 
 def launch_continuously_train(data_dir, label):
-  seed_everything()
-  model = smp.Unet(
-    # encoder_name = "resnet18",
-    encoder_name="timm-mobilenetv3_small_minimal_100",
-    encoder_weights = "imagenet",
-    in_channels = 3,
-    classes = 1,
-    activation = "sigmoid",
-  ).to(DEVICE)
-  # model = smp.Unet(
-  #   encoder_name = "tu-hrnet_w18",
-  #   encoder_weights = "imagenet",
-  #   in_channels = 3,
-  #   classes = 1,
-  #   activation = "sigmoid",
-  # )
+    seed_everything()
+    model = smp.Unet(
+        # encoder_name = "resnet18",
+        encoder_name="timm-mobilenetv3_small_minimal_100",
+        encoder_weights="imagenet",
+        in_channels=3,
+        classes=1,
+        activation="sigmoid",
+    ).to(DEVICE)
+    # model = smp.Unet(
+    #   encoder_name = "tu-hrnet_w18",
+    #   encoder_weights = "imagenet",
+    #   in_channels = 3,
+    #   classes = 1,
+    #   activation = "sigmoid",
+    # )
 
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-  dataset = ContinuousDataset(Path(data_dir) / 'inputs', Path(data_dir) / 'annotations', label)
-  if len(dataset) < 32:
-    print('Waiting for annotations...')
-    for wait in tqdm.tqdm(range(1000)):
-      time.sleep(1)
-      dataset.update_file_list()
-      if len(dataset) >= 32:
-        break
+    dataset = ContinuousDataset(
+        Path(data_dir) / "inputs", Path(data_dir) / "annotations", label
+    )
     if len(dataset) < 32:
-      raise RuntimeError('Waited too long for annotations to start training')
-  print('Starting training...')
-  Path('checkpoints').mkdir(exist_ok=True)
+        print("Waiting for annotations...")
+        for wait in tqdm.tqdm(range(1000)):
+            time.sleep(1)
+            dataset.update_file_list()
+            if len(dataset) >= 32:
+                break
+        if len(dataset) < 32:
+            raise RuntimeError("Waited too long for annotations to start training")
+    print("Starting training...")
+    Path("checkpoints").mkdir(exist_ok=True)
 
-  writer = SummaryWriter()
+    writer = SummaryWriter()
 
-  st = time.time()
-  last = st
-  times = {'dataloader': 0, 'loading_data': 0, 'forward': 0, 'backward': 0, 'logging': 0, 'image_logging': 0, 'model_saving': 0}
-  iterations = 0
-  for epoch in range(100000000):
-    dataset.update_file_list()  # the reason why to use plain pytorch
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True, num_workers=8, drop_last=True,
-            pin_memory=True, prefetch_factor=4,
-            )
+    st = time.time()
+    last = st
+    times = {
+        "dataloader": 0,
+        "loading_data": 0,
+        "forward": 0,
+        "backward": 0,
+        "logging": 0,
+        "image_logging": 0,
+        "model_saving": 0,
+    }
+    iterations = 0
+    for epoch in range(100000000):
+        dataset.update_file_list()  # the reason why to use plain pytorch
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=8,
+            shuffle=True,
+            num_workers=8,
+            drop_last=True,
+            pin_memory=True,
+            prefetch_factor=4,
+        )
 
-    times['dataloader'] += time.time() - last
-    last = time.time()
-
-    with tqdm.tqdm(enumerate(dataloader)) as pbar:
-      for i, (img, target) in pbar:
-        img, target = img.to(DEVICE), target.to(DEVICE)
-
-        times['loading_data'] += time.time() - last
+        times["dataloader"] += time.time() - last
         last = time.time()
 
-        out = model(img)
-        loss = torchvision.ops.sigmoid_focal_loss(out, target[:, None, :, :].float(), reduction='mean')
+        with tqdm.tqdm(enumerate(dataloader)) as pbar:
+            for i, (img, target) in pbar:
+                img, target = img.to(DEVICE), target.to(DEVICE)
 
-        times['forward'] += time.time() - last
+                times["loading_data"] += time.time() - last
+                last = time.time()
+
+                out = model(img)
+                loss = torchvision.ops.sigmoid_focal_loss(
+                    out, target[:, None, :, :].float(), reduction="mean"
+                )
+
+                times["forward"] += time.time() - last
+                last = time.time()
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                times["backward"] += time.time() - last
+                last = time.time()
+
+                pbar.set_description(f"Epoch {epoch}, batch {i}, loss {loss.item()}")
+                writer.add_scalar("Loss/train", loss.item(), global_step=iterations)
+                iterations += 1
+
+                times["logging"] += time.time() - last
+                last = time.time()
+
+            writer.add_image("Image/train/input", img[0], epoch)
+            writer.add_image("Image/train/output", out[0], epoch)
+
+            times["image_logging"] += time.time() - last
+            last = time.time()
+
+        print(f"finished epoch {epoch} with times {times}")
+        torch.save(model, f"checkpoints/model_{str(epoch).zfill(4)}.pth")
+
+        times["model_saving"] += time.time() - last
         last = time.time()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        times['backward'] += time.time() - last
-        last = time.time()
-
-        pbar.set_description(f"Epoch {epoch}, batch {i}, loss {loss.item()}")
-        writer.add_scalar('Loss/train', loss.item(), global_step=iterations)
-        iterations += 1
-
-        times['logging'] += time.time() - last
-        last = time.time()
-
-      writer.add_image('Image/train/input', img[0], epoch)
-      writer.add_image('Image/train/output', out[0], epoch)
-
-      times['image_logging'] += time.time() - last
-      last = time.time()
-
-    print(f"finished epoch {epoch} with times {times}")
-    torch.save(model, f"checkpoints/model_{str(epoch).zfill(4)}.pth")
-
-    times['model_saving'] += time.time() - last
-    last = time.time()
-
-    
-  
-  writer.flush()
-  writer.close()
-   
+    writer.flush()
+    writer.close()
 
 
-if __name__ == '__main__':
-  label = int(sys.argv[1])
-  data_dir = os.path.join(DATA_DIR, 'perlabel_sbd')
-  launch_continuously_train(data_dir, label)
-
-  
+if __name__ == "__main__":
+    label = int(sys.argv[1])
+    data_dir = os.path.join(DATA_DIR, "perlabel_sbd")
+    launch_continuously_train(data_dir, label)
