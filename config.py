eb3ecd2a95823c6a7c07d8042bfe8d65e57add6d
franchesoni <marchesoniacland@gmail.com>
2023-04-12 04:17:47 +0200

    change dataset path

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-DATA_DIR = '/home/franchesoni/data/iaseg_paper/'
+DATA_DIR = '/home/franchesoni/adisk/datasets'
 Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
 DOWNLOAD_IF_NEEDED = True
 SEED = 42
