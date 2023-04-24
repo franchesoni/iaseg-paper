1bc65606cb235d43430f8158063348ba8c1fe6d7
franchesoni <marchesoniacland@gmail.com>
2023-04-24 15:47:11 +0200

    fix data path and downlaod sam weights

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 from pathlib import Path
-DATA_DIR = '/home/franchesoni/adisk/datasets'
-Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
+DATA_DIR = Path('/home/franchesoni/adisk/datasets')
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+NDD20_DIR = DATA_DIR / "NDD20"
 DOWNLOAD_IF_NEEDED = True
 SEED = 42
+SAM_VITL_PATH = Path("weights/sam_vit_l_0b3195.pth")
+if not SAM_VITL_PATH.exists():
+    SAM_VITL_PATH.parent.mkdir(parents=True, exist_ok=True)
+    # run wget and wait until it finishes
+    command = f"wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth -O {str(SAM_VITL_PATH)}"
+    import os
+    os.system(command)
+
 # IMAGE_SIZE = (224, 224)
