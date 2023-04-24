e3afad663abcac795f1dfa8b548173c4f86f07f2
franchesoni <marchesoniacland@gmail.com>
2023-04-24 14:07:13 +0200

    unify functions

--- supervised_baseline.py
+++ supervised_baseline.py
@@ -1,10 +1,10 @@
+import tqdm
 import pytorch_lightning as pl
 from pathlib import Path
 import torchvision
 import torch
 
-from segmentation_datasets.ndd20 import NDD20Dataset
-from segmentation_datasets.ndd20 import transforms
+from segmentation_datasets.ndd20 import NDD20DataModule
 from config import DATA_DIR, SEED
 
 
@@ -18,11 +18,10 @@ class Segmentator(pl.LightningModule):
         self.model.classifier[4] = torch.nn.Conv2d(
             256, 1, kernel_size=(1, 1), stride=(1, 1)
         )  # 1 class
-        breakpoint()
         self.loss = torchvision.ops.sigmoid_focal_loss
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
+        x, y, hw = batch["input"], batch["target"], batch["hw"]
         y_hat = self.model(x)["out"]
         loss = self.loss(y_hat, y, reduction="mean")
         self.log("train_loss", loss)
@@ -44,81 +43,60 @@ class Segmentator(pl.LightningModule):
         return loss
 
 
-def train_segmentator():
+def run_segmentator(mode='train'):
     torch.manual_seed(SEED)
-    segmentator = Segmentator()
-    dataset = NDD20Dataset(
-        root_dir=Path(DATA_DIR) / "NDD20", subdataset="below", split="train"
+    datamodule = NDD20DataModule(
+        data_dir=Path(DATA_DIR) / "NDD20", 
+        batch_size=32,
     )
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=42,
-        shuffle=True,
-        num_workers=8,
-        pin_memory=True,
-        drop_last=True,
-    )
-    trainer = pl.Trainer(
-        accelerator="gpu",
-        max_epochs=48,
-        fast_dev_run=False,
-        devices=1,
-        profiler="simple",
-    )
-    trainer.fit(segmentator, dataloader)
-
-
-def evaluate_segmentator():
-    torch.manual_seed(SEED)
-    segmentator = Segmentator.load_from_checkpoint(
-        checkpoint_path="lightning_logs/version_1/checkpoints/epoch=47-step=1968.ckpt"
-    )
-    dataset = NDD20Dataset(
-        root_dir=Path(DATA_DIR) / "NDD20", subdataset="below", split="test"
-    )
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=42,
-        shuffle=False,
-        num_workers=8,
-        pin_memory=True,
-        drop_last=False,
-    )
-    trainer = pl.Trainer(
-        accelerator="gpu",
-        max_epochs=48,
-        fast_dev_run=False,
-        devices=1,
-        profiler="simple",
-    )
-    trainer.test(segmentator, dataloader)
-
-
-def generate_predictions():
-    torch.manual_seed(SEED)
-    segmentator = Segmentator.load_from_checkpoint(
-        checkpoint_path="lightning_logs/version_1/checkpoints/epoch=47-step=1968.ckpt"
-    )
-    dataset = NDD20Dataset(
-        root_dir=Path(DATA_DIR) / "NDD20",
-        subdataset="below",
-        split="train",
-        transform=None,
-    )
-    segmentator.model.eval()
-    with torch.no_grad():
-        for ind, (original_img, _) in enumerate(dataset):
-            resized_img = transforms(original_img)
-            prediction = torch.sigmoid(
-                segmentator.model(resized_img[None])["out"][0][0]
+    if mode in ['train', 'test']:
+        segmentator = Segmentator()
+        trainer = pl.Trainer(
+            log_every_n_steps=1,
+            accelerator="gpu",
+            max_epochs=24,
+            fast_dev_run=False,
+            devices=[1],
+            profiler="simple",
+        )
+        if mode == 'train':
+            trainer.fit(segmentator, datamodule)
+        elif mode == 'test':
+            trainer.test(segmentator, datamodule)
+    elif mode == 'generate':
+        Path('predictions').mkdir(exist_ok=True)
+        datamodule.setup('fit')
+        segmentator = Segmentator.load_from_checkpoint(
+                checkpoint_path=str(list(Path("lightning_logs/version_5/checkpoints/").glob("*.ckpt"))[0])
             )
-            torchvision.utils.save_image(prediction, f"predictions/{ind}.png")
+        segmentator.model.eval()
 
+        with torch.no_grad():
+            for ind, (sampledict) in tqdm.tqdm(enumerate(datamodule.ndd20_train)):
+                img = sampledict["input"]
+                h, w = sampledict["hw"]
+                prediction = torch.sigmoid(
+                    segmentator.model(img[None])["out"][0][0]
+                )
+                # resize image back to (h, w)
+                prediction = torch.nn.functional.interpolate(
+                    prediction[None, None], size=(h, w)
+                )[0, 0]
+                torchvision.utils.save_image(prediction, f"predictions/{str(ind).zfill(5)}.png")
+
+            indoffset = len(datamodule.ndd20_train)
+            for ind, (sampledict) in tqdm.tqdm(enumerate(datamodule.ndd20_val)):
+                img = sampledict["input"]
+                h, w = sampledict["hw"]
+                prediction = torch.sigmoid(
+                    segmentator.model(img[None])["out"][0][0]
+                )
+                prediction = torch.nn.functional.interpolate(
+                    prediction[None, None], size=(h, w)
+                )[0, 0]
+                torchvision.utils.save_image(prediction, f"predictions/{str(ind+indoffset).zfill(5)}.png")
 
 # command to run the script and log output of terminal to a file
 # python supervised_baseline.py | tee supervised_baseline.log
-
 if __name__ == "__main__":
-    train_segmentator()
-    # evaluate_segmentator()
-    # generate_predictions()
+    run_segmentator('generate')
